61c2de7c6cd2a1faad5dc2ae67a8f77e940bab0e
Azizbekutkirovich <saodat.utkir@gmail.com>
2025-05-05 22:08:46 +0500

    Test 3

--- index.php
+++ index.php
@@ -6,5 +6,8 @@ $telegram = new Telegram('7712252153:AAE9ZG7gCLWT3E3jJCnGOclp82-3OFjO2So');
 
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
+if ($text === "/start") {
+	$text = "Assalomu aleykum. Men dasturchi Safarov Azizbek tomonidan ishlab chiqilgan botman! Sizga qanday yordam bera olaman?";
+}
 $content = array('chat_id' => $chat_id, 'text' => $text);
 $telegram->sendMessage($content);
