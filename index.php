a57d5855a32848cfc011da0f5f73aa303b6834ce
Azizbekutkirovich <saodat.utkir@gmail.com>
2025-05-06 16:45:17 +0500

    Test 9

--- index.php
+++ index.php
@@ -8,14 +8,11 @@ $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 if ($text === "/start") {
 	$option = array(
-    array($telegram->buildKeyboardButton("🤖 Bot imkoniyatlari")),
-    array($telegram->buildKeyboardButton("🛈 Batafsil ma'lumot")));
+    array($telegram->buildKeyboardButton("🛈 Batafsil ma'lumot"), $telegram->buildKeyBoardButton("📄 Rezyume")),
+    array($telegram->buildKeyboardButton("📞 Bog'lanish uchun"), $telegram->buildKeyBoardButton("🤖 Bot zakaz qilish")));
     $keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
-	$content = array('chat_id' => $chat_id, 'text' => "Assalomu aleykum. Men dasturchi Safarov Azizbek tomonidan ishlab chiqilgan botman!");
+	$content = array('chat_id' => $chat_id, 'text' => "Assalomu aleykum. Men dasturchi Safarov Azizbek haqida ma'lumot bera olaman!");
 	$telegram->sendMessage($content);
-	$content = array('chat_id' => $chat_id, "reply_markup" => $keyb, 'text' => "Sizga qanday yordam bera olaman?");
-	$telegram->sendMessage($content);
-} else if ($text === "🛈 Batafsil ma'lumot") {
-	$content = array('chat_id' => $chat_id, 'text' => "Batafsil ma'lumot <a href='https://telegra.ph/Biz-haqimizda-05-06'>Havola</a>", 'parse_mode' => 'html');
+	$content = array('chat_id' => $chat_id, "reply_markup" => $keyb, 'text' => "Qanday ma'lumot kerak?");
 	$telegram->sendMessage($content);
 }
