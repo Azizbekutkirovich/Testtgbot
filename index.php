017c41689b6a21fac341b56316608fbadb6d7040
Azizbekutkirovich <saodat.utkir@gmail.com>
2025-05-09 22:45:30 +0500

    Test 43

--- index.php
+++ index.php
@@ -4,95 +4,98 @@ require_once "Telegram.php";
 require_once "db.php";
 
 
-$telegram = new Telegram('7712252153:AAE9ZG7gCLWT3E3jJCnGOclp82-3OFjO2So');
+$query = $pdo->prepare("INSERT INTO test (id, name) VALUES (?, ?)");
+$query->execute([1, "test"]);
 
-$data = $telegram->getData();
-$message = $data['message'];
-$chat_id = $message['chat']['id'];
-$text = $message['text'];
+// $telegram = new Telegram('7712252153:AAE9ZG7gCLWT3E3jJCnGOclp82-3OFjO2So');
 
-switch ($text) {
-	case "/start":
-		start();
-		break;
-	case "🛈 Batafsil ma'lumot":
-		detail();
-		break;
-	case "📄 Rezyume":
-		rezyume();
-		break;
-	case "📞 Bog'lanish uchun":
-		contact();
-		break;
-	case "🤖 Bot zakaz qilish":
-		zakazBot();
-		break;
-	case "🔙 Ortga qaytish":
-		start();
-		break;
-	default:
-		if ((!empty($message['entities']) && $message['entities'][0]['type'] === "phone_number") || !empty($message['contact'])) {
-			$userId = $message['from']['id'];
-			// $phoneNumber = $message['contact']['phone_number'] ?? $message['text'];
-			$query = $pdo->prepare("INSERT INTO test (id, name) VALUES (?, ?)");
-			$query->execute([$userId, "test"]);
-			$telegram->sendMessage([
-				"chat_id" => $chat_id,
-				"text" => "Tez orada siz bilan bog'lanishadi!"
-			]);
-		}
-		break;
-}
+// $data = $telegram->getData();
+// $message = $data['message'];
+// $chat_id = $message['chat']['id'];
+// $text = $message['text'];
 
-function start() {
-	global $chat_id, $telegram, $message;
-	$first_name = $message['chat']['first_name'] ?? "";
-	$last_name = $message['chat']['last_name'] ?? "";  
-	$option = array(
-	    array($telegram->buildKeyboardButton("🛈 Batafsil ma'lumot"), $telegram->buildKeyboardButton("📄 Rezyume")),
-	    array($telegram->buildKeyboardButton("📞 Bog'lanish uchun"), $telegram->buildKeyboardButton("🤖 Bot zakaz qilish")));
-    $keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
-	$content = array('chat_id' => $chat_id, 'text' => "Assalomu aleykum $last_name $first_name. Men dasturchi Safarov Azizbek haqida ma'lumot bera olaman!");
-	$telegram->sendMessage($content);
-	$content = array('chat_id' => $chat_id, "reply_markup" => $keyb, 'text' => "Qanday ma'lumot kerak?");
-	$telegram->sendMessage($content);
-}
+// switch ($text) {
+// 	case "/start":
+// 		start();
+// 		break;
+// 	case "🛈 Batafsil ma'lumot":
+// 		detail();
+// 		break;
+// 	case "📄 Rezyume":
+// 		rezyume();
+// 		break;
+// 	case "📞 Bog'lanish uchun":
+// 		contact();
+// 		break;
+// 	case "🤖 Bot zakaz qilish":
+// 		zakazBot();
+// 		break;
+// 	case "🔙 Ortga qaytish":
+// 		start();
+// 		break;
+// 	default:
+// 		if ((!empty($message['entities']) && $message['entities'][0]['type'] === "phone_number") || !empty($message['contact'])) {
+// 			$userId = $message['from']['id'];
+// 			// $phoneNumber = $message['contact']['phone_number'] ?? $message['text'];
+// 			$query = $pdo->prepare("INSERT INTO test (id, name) VALUES (?, ?)");
+// 			$query->execute([$userId, "test"]);
+// 			$telegram->sendMessage([
+// 				"chat_id" => $chat_id,
+// 				"text" => "Tez orada siz bilan bog'lanishadi!"
+// 			]);
+// 		}
+// 		break;
+// }
 
-function detail() {
-	global $chat_id, $telegram;
-	$option = [
-		[$telegram->buildKeyboardButton("🔙 Ortga qaytish")]
-	];
-	$keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
-	$content = array('chat_id' => $chat_id, 'text' => "Batafsil ma'lumot uchun havola: <a href='https://telegra.ph/Biz-haqimizda-05-06'>Havola</a>", "parse_mode" => "html", "reply_markup" => $keyb);
-	$telegram->sendMessage($content);
-}
+// function start() {
+// 	global $chat_id, $telegram, $message;
+// 	$first_name = $message['chat']['first_name'] ?? "";
+// 	$last_name = $message['chat']['last_name'] ?? "";  
+// 	$option = array(
+// 	    array($telegram->buildKeyboardButton("🛈 Batafsil ma'lumot"), $telegram->buildKeyboardButton("📄 Rezyume")),
+// 	    array($telegram->buildKeyboardButton("📞 Bog'lanish uchun"), $telegram->buildKeyboardButton("🤖 Bot zakaz qilish")));
+//     $keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
+// 	$content = array('chat_id' => $chat_id, 'text' => "Assalomu aleykum $last_name $first_name. Men dasturchi Safarov Azizbek haqida ma'lumot bera olaman!");
+// 	$telegram->sendMessage($content);
+// 	$content = array('chat_id' => $chat_id, "reply_markup" => $keyb, 'text' => "Qanday ma'lumot kerak?");
+// 	$telegram->sendMessage($content);
+// }
 
-function contact() {
-	global $chat_id, $telegram;
-	$content = array('chat_id' => $chat_id, 'text' => "
-		📍 Адрес: Toshkent shahar Yangi hayot tumani Ibrat 2-tor ko'cha 38
+// function detail() {
+// 	global $chat_id, $telegram;
+// 	$option = [
+// 		[$telegram->buildKeyboardButton("🔙 Ortga qaytish")]
+// 	];
+// 	$keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
+// 	$content = array('chat_id' => $chat_id, 'text' => "Batafsil ma'lumot uchun havola: <a href='https://telegra.ph/Biz-haqimizda-05-06'>Havola</a>", "parse_mode" => "html", "reply_markup" => $keyb);
+// 	$telegram->sendMessage($content);
+// }
 
-	 	📞 Телефон: +998(93)315-23-70
+// function contact() {
+// 	global $chat_id, $telegram;
+// 	$content = array('chat_id' => $chat_id, 'text' => "
+// 		📍 Адрес: Toshkent shahar Yangi hayot tumani Ibrat 2-tor ko'cha 38
 
-	 	✉ Email: azizbek250607@gmail.com
+// 	 	📞 Телефон: +998(93)315-23-70
 
-	 	🐙 GitHub: https://github.com/Azizbekutkirovich/");
-	$telegram->sendMessage($content);
-}
+// 	 	✉ Email: azizbek250607@gmail.com
 
-function zakazBot() {
-	global $chat_id, $telegram;
-	$option = [
-		[$telegram->buildKeyboardButton("Raqam qoldirish", true)]
-	];
-	$keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
-	$content = array("chat_id" => $chat_id, "text" => "Telefon raqamingizni yozib qoldiring! Tez orada siz bilan bog'lanishadi!", "reply_markup" => $keyb);
-	$telegram->sendMessage($content);
-}
+// 	 	🐙 GitHub: https://github.com/Azizbekutkirovich/");
+// 	$telegram->sendMessage($content);
+// }
 
-function rezyume() {
-	global $chat_id, $telegram;
-	$content = array("chat_id" => $chat_id, "text" => "Rezyume tez orada qo'shiladi!");
-	$telegram->sendMessage($content);
-}
+// function zakazBot() {
+// 	global $chat_id, $telegram;
+// 	$option = [
+// 		[$telegram->buildKeyboardButton("Raqam qoldirish", true)]
+// 	];
+// 	$keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
+// 	$content = array("chat_id" => $chat_id, "text" => "Telefon raqamingizni yozib qoldiring! Tez orada siz bilan bog'lanishadi!", "reply_markup" => $keyb);
+// 	$telegram->sendMessage($content);
+// }
+
+// function rezyume() {
+// 	global $chat_id, $telegram;
+// 	$content = array("chat_id" => $chat_id, "text" => "Rezyume tez orada qo'shiladi!");
+// 	$telegram->sendMessage($content);
+// }
