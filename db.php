652aaaa0dd000a482b6b03bcc06b413a3b6e3247
Azizbekutkirovich <saodat.utkir@gmail.com>
2025-05-09 19:24:23 +0500

    Test 28

--- db.php
+++ db.php
@@ -1,15 +1,19 @@
 <?php
 
-$host = "mysql.railway.internal";
-$port = 3306;
-$db_name = "railway";
-$user = "root";
-$password = "sBiQquNTTEiJeLsqogEtVIqBfZCIjCnO";
+$url = getenv('MYSQL_URL');
+
+$parts = parse_url($url);
+$host = $parts['host'];
+$port = $parts['port'];
+$user = $parts['user'];
+$pass = $parts['pass'];
+$db   = ltrim($parts['path'], '/');
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
 try {
-	$pdo = new PDO("mysql:host=$host;port=$port;dbname=$db_name;charset=utf8mb4", $user, $password);
-	
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-	echo "Xatolik: ".$e->getMessage();
-	exit;
+    die("Ulanishda xatolik: " . $e->getMessage());
 }
